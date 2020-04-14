e30dac2a5dbe97f2dd137687230a400e50fc14d0
Greg Harms <slwriotcontrol@gmail.com>
2020-04-14 13:13:52 -0600

    Created simple web server with actix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+extern crate juniper;
+
+use std::io;
+use actix_web::{web, App, Responder, HttpResponse, HttpServer};
+
+fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world")
+}
+
+fn main() -> io::Result<()> {
+    HttpServer::new( || {
+        App::new()
+            .route("/", web::get().to(index))
+    } )
+    .bind("localhost:8080")?
+    .run()
 }
